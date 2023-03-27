75c3f8099d427beb8ec133b979759a59fbbaba3b
Kiruidyno <kirui472@gmail.com>
2023-03-27 10:28:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function renderBooks(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function renderBooks(data) {
+      console.log(data);
+    });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
